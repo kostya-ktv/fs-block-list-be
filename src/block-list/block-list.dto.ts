9c4893bc2cc36aadc4d451e9b6a663dc1d9ab3b6
Kostya <54177203+kostya-ktv@users.noreply.github.com>
2024-01-07 09:35:05 +0200

    [upd] block list dto

---
+++ src/block-list/block-list.dto.ts
@@ -0,0 +1,50 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { $Enums } from '@prisma/client';
+import { IsIn, IsString } from 'class-validator';
+
+const BlockItemType = [
+  $Enums.BlockItemType.KeyWord,
+  $Enums.BlockItemType.Website,
+];
+export class BlockListItemDTO {
+  @ApiProperty()
+  id: number;
+  @ApiProperty()
+  blockListId: number;
+
+  @ApiProperty({
+    type: [BlockItemType],
+  })
+  type: $Enums.BlockItemType;
+
+  @ApiProperty()
+  data: string;
+
+  @ApiProperty({
+    type: Date,
+  })
+  createdAt: Date;
+}
+
+export class BlockListDTO {
+  @ApiProperty()
+  id: number;
+  @ApiProperty()
+  ownerId: number;
+  @ApiProperty({
+    type: [BlockListItemDTO],
+  })
+  items: BlockListItemDTO[];
+}
+
+export class BlockListItemCreateDTO {
+  @ApiProperty({
+    type: [BlockItemType],
+  })
+  @IsIn([BlockItemType])
+  type: $Enums.BlockItemType;
+
+  @ApiProperty()
+  @IsString()
+  data: string;
+}
